a0b54d658c23a65a8eaa9f8a33de19c509a288a7
Anirudh4583 <anirudhmitra210@gmail.com>
2021-10-13 10:24:04 +0530

    ERN
    react express node basic setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import compression from 'compression'
+import cors from 'cors'
 
-// Create Express server
 const app = express()
 
 app.set('port', process.env.PORT || 3001)
+app.use(cors())
+app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/hello', (req: Request, res: Response): void => {
+  res.status(200).send('hello world! this is working sheeeeesh')
+})
 
 export default app
