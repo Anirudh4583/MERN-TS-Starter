90e161628430c8baff63f915ffdb61e3f296f4aa
Anirudh4583 <anirudhmitra210@gmail.com>
2021-10-13 15:59:32 +0530

    to be continued

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from 'express'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import { MONGODB_URI } from './util/secrets'
 
 const app = express()
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err) => {
+    console.log(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`,
+    )
+    // process.exit();
+  })
 
 app.set('port', process.env.PORT || 3001)
 app.use(cors())
